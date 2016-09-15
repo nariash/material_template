5326089a1f54ff7e9e78175c3b28bda367f483bc
Nelson Arias Hidalgo <nelson@cfrd.cl>
2016-09-15 15:43:27 -0300

    Frontpage con colapsable horizontal de cuatro módulos y barra de navegación superior. Diseño responsivo.

--- js/index.js
+++ js/index.js
@@ -5,4 +5,13 @@ $(document).ready(function(){
 	$('.include').each(function(){
 		$(this).load( $(this).data('include') );
 	})
+
+	$('#semestres').on('click', '.semestre:not(.block):not(.active)', function(){
+		$('.semestre.active').removeClass('active m6').addClass('m2')
+		$(this).removeClass('m2').addClass('active m6')
+	});
+
+	/*$('.semestre').each(function(i){
+		$(this).prepend( $('<span />',{'text':'SEMESTRE'}) )
+	})*/
 });
